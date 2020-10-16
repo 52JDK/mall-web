1dfe617a7d9137c6d766898f950901ac32875e1d
申浩 <20016402@lp.com>
2020-10-16 11:43:27 +0800

    提交注释

--- vue.config.js
+++ vue.config.js
@@ -10,13 +10,19 @@ module.exports = {
     lintOnSave: false,
     filenameHashing: true,
     productionSourceMap: false,
+    // ...
     configureWebpack: {
-        resolve: {
-          alias: {
-            '@': resolve('src')
-          }
-        }
-      },
+        plugins: [
+            new CompressionPlugin({
+                algorithm: 'gzip', // 使用gzip压缩
+                test: /\.js$|\.html$|\.css$/, // 匹配文件名
+                filename: '[path].gz[query]', // 压缩后的文件名(保持原文件名，后缀加.gz)
+                minRatio: 1, // 压缩率小于1才会压缩
+                threshold: 10240, // 对超过10k的数据压缩
+                deleteOriginalAssets: false, // 是否删除未压缩的源文件，谨慎设置，如果希望提供非gzip的资源，可不设置或者设置为false（比如删除打包后的gz后还可以加载到原始资源文件）
+            }),
+        ],
+    },
     css: {
         loaderOptions: {
             css: {},
